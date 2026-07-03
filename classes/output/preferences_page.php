ddb38fd7b2a55f2eca4915be0767a66c17050f8a
Jean Lúcio <jeanlucio@gmail.com>
2026-07-03 09:03:20 +0000

    feat: phase 3 - implement preferences page and output renderer

---
+++ classes/output/preferences_page.php
@@ -0,0 +1,110 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Output renderable for the preferences page.
+ *
+ * @package    local_plugwatch
+ * @copyright  2026 Jean Lúcio
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_plugwatch\output;
+
+use renderable;
+use templatable;
+use renderer_base;
+use local_plugwatch\local\watchlist_manager;
+
+/**
+ * Preferences page renderable.
+ *
+ * @package    local_plugwatch
+ * @copyright  2026 Jean Lúcio
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class preferences_page implements renderable, templatable {
+    /** @var int User ID */
+    private int $userid;
+
+    /** @var int Selected frequency */
+    private int $frequency;
+
+    /**
+     * Constructor.
+     *
+     * @param int $userid The user ID.
+     * @param int|null $frequency The selected frequency (optional, fallbacks to user preference).
+     */
+    public function __construct(int $userid, ?int $frequency = null) {
+        $this->userid = $userid;
+        if ($frequency !== null) {
+            $this->frequency = $frequency;
+        } else {
+            $this->frequency = (int) get_user_preferences('local_plugwatch_frequency', 604800, $userid);
+        }
+    }
+
+    /**
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @param renderer_base $output The renderer.
+     * @return array Context array.
+     */
+    public function export_for_template(renderer_base $output): array {
+        global $CFG;
+
+        $watchlist = watchlist_manager::get_user_plugins($this->userid);
+        $maxplugins = (int) get_config('local_plugwatch', 'maxplugins');
+        
+        $plugins = [];
+        foreach ($watchlist as $item) {
+            $plugins[] = [
+                'component' => $item->component,
+                'releasename' => $item->releasename ?? '',
+                'timelastnotified' => $item->timelastnotified ? userdate($item->timelastnotified) : '',
+                'timechecked' => $item->timechecked ? userdate($item->timechecked) : '',
+            ];
+        }
+
+        $frequencies = [
+            86400 => get_string('frequency_daily', 'local_plugwatch'),
+            604800 => get_string('frequency_weekly', 'local_plugwatch'),
+            2592000 => get_string('frequency_monthly', 'local_plugwatch'),
+        ];
+
+        $frequencyoptions = [];
+        foreach ($frequencies as $val => $label) {
+            $frequencyoptions[] = [
+                'value' => $val,
+                'label' => $label,
+                'selected' => ($this->frequency === $val),
+            ];
+        }
+
+        return [
+            'sesskey' => sesskey(),
+            'wstoken' => '', // Actually, WS are session-based with Ajax.call(), no wstoken needed in DOM here.
+            'maxplugins' => $maxplugins,
+            'currentcount' => count($plugins),
+            'frequency' => $this->frequency,
+            'frequencyoptions' => $frequencyoptions,
+            'hasplugins' => !empty($plugins),
+            'plugins' => $plugins,
+            'notificationprefsurl' => new \moodle_url('/message/notificationpreferences.php'),
+        ];
+    }
+}
---
+++ preferences.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Entry point for the preferences page.
+ *
+ * @package    local_plugwatch
+ * @copyright  2026 Jean Lúcio
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../config.php');
+
+require_login();
+$context = context_system::instance();
+require_capability('local/plugwatch:use', $context);
+
+$url = new moodle_url('/local/plugwatch/preferences.php');
+$PAGE->set_url($url);
+$PAGE->set_context($context);
+$PAGE->set_pagelayout('admin');
+$PAGE->set_title(get_string('preferences_heading', 'local_plugwatch'));
+$PAGE->set_heading(get_string('preferences_heading', 'local_plugwatch'));
+
+// Handle form submission for frequency change.
+if ($action = optional_param('action', '', PARAM_ALPHANUM)) {
+    require_sesskey();
+    if ($action === 'setfrequency') {
+        $frequency = required_param('frequency', PARAM_INT);
+        set_user_preference('local_plugwatch_frequency', $frequency, $USER);
+        redirect($url, get_string('changessaved', 'core'), null, \core\output\notification::NOTIFY_SUCCESS);
+    }
+}
+
+echo $OUTPUT->header();
+
+$renderable = new \local_plugwatch\output\preferences_page((int) $USER->id);
+$renderer = $PAGE->get_renderer('local_plugwatch');
+echo $renderer->render($renderable);
+
+$PAGE->requires->js_call_amd('local_plugwatch/preferences', 'init', ['.path-local-plugwatch']);
+
+echo $OUTPUT->footer();
